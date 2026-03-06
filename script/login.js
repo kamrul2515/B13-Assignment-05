4c8f0989f97ca7ec8295f00905f743d5e57a007c
kamrul2515 <kamrulhasansujon2515@gmail.com>
2026-03-06 21:45:28 +0600

    login page added

---
+++ script/login.js
@@ -0,0 +1,7 @@
+document.getElementById("login-btn")
+.addEventListener("click", function(){
+    // 1.get the username input
+    const usernameInput = document.getElementById("input-username");
+    const username = usernameInput.value;
+    console.log(username)
+})
