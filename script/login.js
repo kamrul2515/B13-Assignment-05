f75076f0c505ee7726ca0f57c39cd4a7b415c2c1
kamrul2515 <kamrulhasansujon2515@gmail.com>
2026-03-06 21:57:52 +0600

    loginjs and homepage added

--- script/login.js
+++ script/login.js
@@ -3,5 +3,22 @@ document.getElementById("login-btn")
     // 1.get the username input
     const usernameInput = document.getElementById("input-username");
     const username = usernameInput.value;
-    console.log(username)
-})
+    console.log(username);
+    // 2. get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+    // 3. match pin and username
+    if(username == "admin" && pin =="admin123"){
+        // true:::>> alert> homepage
+        alert("Login Successfull");
+
+        window.location.assign("/home.html");
+
+    }
+    else{
+        // false:::>> alert> invalid
+        alert("Login Failed");
+        return;
+    }
+});
